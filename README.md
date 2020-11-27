5f8ac63e714bec15747cd1816384e1e2c4055fdd
lcl9985 <liancl@dragoninfo.com.cn>
2020-11-27 10:49:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+###添加
+      滴滴插件只支持  tools 3.1.0   gradle-4.6-all.zip  android 9  所以另外起一个项目      targetHost 自己配置                       
---
+++ app/src/main/java/com/example/pluginddtest/PluginMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pluginddtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PluginMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plugin_main)
+    }
+}
